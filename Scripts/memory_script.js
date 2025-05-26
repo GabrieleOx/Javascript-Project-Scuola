12235a1daafdcdec5a1c7d3a45c796c664e9141a
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2025-05-26 18:25:19 +0200

    Aggiunta rotazione, download punteggio e barra

--- Scripts/memory_script.js
+++ Scripts/memory_script.js
@@ -4,6 +4,11 @@ const carte = document.getElementById("containerCarte");
 const vaso = document.getElementById("containerVaso");
 const retry = document.getElementById("retryButton");
 const score = document.getElementById("punteggioEffettuato");
+const tendina = document.getElementById("tendina-button");
+const barra = document.getElementById("barra-fissa");
+const titolo = document.getElementById("titoloPagina");
+const save = document.getElementById("saveButton");
+const saveLink = document.getElementById("saveLink");
 
 var associamenti = new Array(20);
 var immagini = new Array(10);
@@ -15,6 +20,8 @@ var done = "Imgs/Done.png";
 var puntoInterrogativo = "Imgs/Interrogativo.png";
 var mosse = 0, punteggio = 100;
 var completati = 0;
+var tenda_giu = false;
+var cardIds = new Array(2);
 
 startButton.addEventListener("click", function(){
     sacchetto.style.display = 'none';
@@ -41,7 +48,7 @@ startButton.addEventListener("click", function(){
             left = 220;
             top += 230;
         }
-        addListeners(immagineFinita, associamenti[(i-1)], (i-1));
+        addListeners(immagineFinita, associamenti[(i-1)], (i-1), card);
     }
 
 });
@@ -86,7 +93,7 @@ function setAssociazioni(){
     }
 }
 
-function addListeners(id, cardN, numAssoc){
+function addListeners(id, cardN, numAssoc, idCarta){
     document.getElementById(id).addEventListener("click", function(){
         if(associamenti[numAssoc] != 0 && toccati < 2 && numeriAssociati[0] != numAssoc){
             toccati++;
@@ -95,19 +102,36 @@ function addListeners(id, cardN, numAssoc){
                 giratiN[0] = cardN;
                 giratiId[0] = id;
                 numeriAssociati[0] = numAssoc;
+                cardIds[0] = idCarta;
 
-                document.getElementById(giratiId[0]).src = immagini[giratiN[0]];
+                cardIds[0].style.transform = "rotateY(180deg)";
+
+                let im = document.getElementById(giratiId[0]);
+                
+                setTimeout(() =>{
+                    im.src = immagini[giratiN[0]];
+                    im.style.transform = "rotateY(180deg)";
+                }, 98);
             }else {
                 giratiN[1] = cardN;
                 giratiId[1] = id;
                 numeriAssociati[1] = numAssoc;
+                cardIds[1] = idCarta;
 
-                document.getElementById(giratiId[1]).src = immagini[giratiN[1]];
+                cardIds[1].style.transform = "rotateY(180deg)";
+
+                let im0 = document.getElementById(giratiId[0]);
+                let im1 = document.getElementById(giratiId[1]);
+                
+                setTimeout(() =>{
+                    im1.src = immagini[giratiN[1]];
+                    im1.style.transform = "rotateY(180deg)";
+                }, 98);
 
                 if(giratiN[0] == giratiN[1]){
                     setTimeout(() => {
-                        document.getElementById(giratiId[0]).src = done;
-                        document.getElementById(giratiId[1]).src = done;
+                        im0.src = done;
+                        im1.src = done;
                         toccati = 0;
                     }, 500);
                     associamenti[numeriAssociati[0]] = 0;
@@ -115,8 +139,17 @@ function addListeners(id, cardN, numAssoc){
                     completati++;
                 }else {
                     setTimeout(() => {
-                        document.getElementById(giratiId[0]).src = puntoInterrogativo;
-                        document.getElementById(giratiId[1]).src = puntoInterrogativo;
+                        cardIds[0].style.transform = "rotateY(0deg)";
+                        cardIds[1].style.transform = "rotateY(0deg)";
+
+                        setTimeout(() => {
+                            im0.style.transform = "rotateY(0deg)";
+                            im1.style.transform = "rotateY(0deg)";
+
+                            im0.src = puntoInterrogativo;
+                            im1.src = puntoInterrogativo;
+                        }, 98);
+                        
                         toccati = 0;
                     }, 2000);
                 }
@@ -126,6 +159,19 @@ function addListeners(id, cardN, numAssoc){
                 carte.style.display = 'none';
                 score.textContent = 'Score: ' + punteggio;
                 vaso.style.display = 'block';
+
+                retry.style.display = 'none';
+                save.style.display = 'none';
+
+                const corpo = document.body;
+
+                html2canvas(corpo).then(canvas => {
+                    saveLink.download = 'Score.png';
+                    saveLink.href = canvas.toDataURL("image/png");
+                });
+                
+                retry.style.display = 'block';
+                save.style.display = 'block';
             }
         }
     });
@@ -138,13 +184,11 @@ function updatePunteggio(){
 }
 
 startButton.addEventListener("mouseover", function(){
-    startButton.style.backgroundSize = '180px 80px';
-    startButton.style.fontSize = '24px';
+    startButton.style.transform = 'scale(1.15)';
 });
 
 startButton.addEventListener("mouseleave", function(){
-    startButton.style.backgroundSize = '160px 60px';
-    startButton.style.fontSize = '22px';
+    startButton.style.transform = 'scale(1)';
 });
 
 retry.addEventListener("click", function(){
@@ -153,4 +197,18 @@ retry.addEventListener("click", function(){
     completati = 0;
     punteggio = 100;
     startButton.click();
+});
+
+tendina.addEventListener("click", function(){
+    if(tenda_giu){
+        tendina.style.transform = "translateY(0px)";
+        barra.style.transform = "translateY(0px)";
+        titolo.style.transform = "translateY(0px)";
+    }else {
+        tendina.style.transform = "translateY(90px)";
+        barra.style.transform = "translateY(90px)";
+        titolo.style.transform = "translateY(40px)";
+    }
+
+    tenda_giu = !tenda_giu;
 });
